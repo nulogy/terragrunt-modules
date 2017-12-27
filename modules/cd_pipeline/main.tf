68413a5858314f20dd77654ad29582b11a2602b2
Arturo Pie <arturop@nulogy.com>
2017-12-27 14:26:45 -0500

    GO-220: does not create access key for cd user because we also need the secret key

--- modules/cd_pipeline/main.tf
+++ modules/cd_pipeline/main.tf
@@ -2,10 +2,6 @@ resource "aws_iam_user" "user" {
   name = "${var.environment_name}-env-deployer"
 }
 
-resource "aws_iam_access_key" "user_key" {
-  user = "${aws_iam_user.user.name}"
-}
-
 resource "aws_iam_group" "group" {
   name = "${var.environment_name}-env-deployer"
 }

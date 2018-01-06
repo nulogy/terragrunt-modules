b31b9ca73cdee8d8429b83de04cd48ffb6aabbe4
Rafael Moraes <rafaelm@nulogy.com>
2018-01-06 15:31:05 -0500

    enables dns resolution on the accepter vpc

--- modules/vpc_peering_connection/vpc_peering_connection.tf
+++ modules/vpc_peering_connection/vpc_peering_connection.tf
@@ -6,6 +6,10 @@ resource "aws_vpc_peering_connection" "vpc_peering_connection" {
   vpc_id        = "${var.vpc_id}"
   auto_accept   = "${var.auto_accept}"
 
+  accepter {
+    allow_remote_vpc_dns_resolution = true
+  }
+
   requester {
     allow_remote_vpc_dns_resolution = true
   }

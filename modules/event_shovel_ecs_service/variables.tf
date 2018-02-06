c55cd4e312202424cec429fa277b90fa0f890aa8
Arturo Pie <arturop@nulogy.com>
2018-02-06 14:10:40 -0500

    GO-198: removes a variable is not used anymore

--- modules/event_shovel_ecs_service/variables.tf
+++ modules/event_shovel_ecs_service/variables.tf
@@ -10,5 +10,4 @@ variable "kms_key_id" { default = "" }
 variable "log_group_name" {}
 variable "memory_reservation" {}
 variable "param_store_namespace" {}
-variable "redis_memory_reservation" {}
 variable "skip" { default = "" }

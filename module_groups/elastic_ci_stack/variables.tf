1641df87524245721c3792186e6b307423d74a88
Arturo Pie <arturop@nulogy.com>
2018-04-20 09:10:54 -0400

    PM-1359: allows customization of the agent ami id since our docker build is no compatible with the latest one due to enabled userns

--- module_groups/elastic_ci_stack/variables.tf
+++ module_groups/elastic_ci_stack/variables.tf
@@ -1,5 +1,5 @@
 variable "buildkite_agent_token" { }
-variable "buildkite_api_access_token" {}
 variable "buildkite_env_name" { description = "Use to prefix resource names and buildkite queue names" }
-variable "stack_template_url" { description = "URL pointing to the buildkite aws cloud formation template" }
+variable "stack_ami_version" { }
 variable "stack_config_env" { description = "Environment file for all buildkite pipelines" }
+variable "stack_template_url" { description = "URL pointing to the buildkite aws cloud formation template" }

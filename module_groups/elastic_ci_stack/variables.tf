5fa069f516e5834d393103301dd4325af34adb86
Arturo Pie <arturop@nulogy.com>
2018-04-20 15:28:41 -0400

    makes stack ami version variable optional in the module group for buildkite stack

--- module_groups/elastic_ci_stack/variables.tf
+++ module_groups/elastic_ci_stack/variables.tf
@@ -1,5 +1,5 @@
 variable "buildkite_agent_token" { }
 variable "buildkite_env_name" { description = "Use to prefix resource names and buildkite queue names" }
-variable "stack_ami_version" { }
+variable "stack_ami_version" { default = "" }
 variable "stack_config_env" { description = "Environment file for all buildkite pipelines" }
 variable "stack_template_url" { description = "URL pointing to the buildkite aws cloud formation template" }

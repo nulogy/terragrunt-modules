b61857ad286805312af4e2ff3ff6df106497248d
Cameron Ross <cameronr@nulogy.com>
2018-04-24 09:10:26 -0400

    bump to version 0.11.5

--- module_groups/elastic_ci_stack/variables.tf
+++ module_groups/elastic_ci_stack/variables.tf
@@ -1,5 +1,9 @@
 variable "buildkite_agent_token" { }
 variable "buildkite_env_name" { description = "Use to prefix resource names and buildkite queue names" }
+variable "runner_instance_type" {}
+variable "runner_max_size" {}
+variable "runner_scale_adjustment" {}
+variable "runner_spot_price" {}
 variable "stack_ami_version" { default = "" }
 variable "stack_config_env" { description = "Environment file for all buildkite pipelines" }
 variable "stack_template_url" { description = "URL pointing to the buildkite aws cloud formation template" }

47b13a8cc3af7fda73c0aedd1073c5b04bdb4489
Arturo Pie <arturop@nulogy.com>
2019-09-05 12:22:16 -0400

    PM-5279: removes redirects as a healthy response for the load balancer health check

--- modules/public_load_balancer/load_balancer.tf
+++ modules/public_load_balancer/load_balancer.tf
@@ -26,7 +26,7 @@ resource "aws_lb_target_group" "target_group" {
   health_check {
     path = "${var.health_check_path}"
     timeout = "${var.health_check_timeout}"
-    matcher = "200,301"
+    matcher = "200"
   }
 
   stickiness {

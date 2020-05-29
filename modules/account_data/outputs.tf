660bc8a77488d1ac5dbd301e0d85554d285c5785
Jason Schweier <jasons@nulogy.com>
2020-05-29 10:23:20 -0400

    TASS-99: Reorder outputs

--- modules/account_data/outputs.tf
+++ modules/account_data/outputs.tf
@@ -10,16 +10,16 @@ output "account_dns_zone_name" {
   value = data.aws_route53_zone.account_zone.name
 }
 
-output "shared_vpc_id" {
-  value = data.aws_vpc.vpc.id
+output "shared_private_subnet_ids" {
+  value = data.aws_subnet_ids.private_subnets.ids
 }
 
 output "shared_public_subnet_ids" {
   value = data.aws_subnet_ids.public_subnets.ids
 }
 
-output "shared_private_subnet_ids" {
-  value = data.aws_subnet_ids.private_subnets.ids
+output "shared_vpc_id" {
+  value = data.aws_vpc.vpc.id
 }
 
 output "shared_vpc_cidr" {

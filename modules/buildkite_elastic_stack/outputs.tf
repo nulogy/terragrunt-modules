d0e4ff994e2e0f72a2bfd20a48bda5dd499174d5
Arturo Pie <arturop@nulogy.com>
2020-06-11 15:18:23 -0400

    TASS-140: adds an output to buildkite elastic stack for getting the instance role name

--- modules/buildkite_elastic_stack/outputs.tf
+++ modules/buildkite_elastic_stack/outputs.tf
@@ -2,3 +2,6 @@ output "security_group_id" {
   value = aws_security_group.stack_security_group.id
 }
 
+output "instance_role_name" {
+  value = aws_cloudformation_stack.stack.outputs["InstanceRoleName"]
+}

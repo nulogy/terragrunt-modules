e2017d073eed81d87a6148cc0dfd636d60b0427c
Adam Kerr <ajrkerr@gmail.com>
2020-07-14 15:37:56 -0400

    TASS-38: Remove more unnecessary configuration

--- modules/vpc_peering/setup.tf
+++ modules/vpc_peering/setup.tf
@@ -1,17 +1,9 @@
 provider "aws" {
-  profile = "nulogy-anchor"
   alias   = "requester"
   version = "~> 2.0"
-  assume_role {
-    role_arn = "arn:aws:iam::${var.requester_account_id}:role/OrganizationAccountAccessRole"
-  }
 }
 
 provider "aws" {
-  profile = "nulogy-anchor"
   alias   = "acceptor"
   version = "~> 2.0"
-  assume_role {
-    role_arn = "arn:aws:iam::${var.acceptor_account_id}:role/OrganizationAccountAccessRole"
-  }
 }

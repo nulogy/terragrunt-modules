bce56c8baea555976b9cd7109f52ffca895b3edc
Adam Kerr <ajrkerr@gmail.com>
2020-10-02 13:36:43 -0400

    TASS-388: Add outputs

---
+++ modules/debezium_database_user/outputs.tf
@@ -0,0 +1,11 @@
+output "debezium_username" {
+  value = var.debezium_username
+}
+
+output "debezium_password" {
+  value = var.debezium_password
+}
+
+output "debezium_events_table" {
+  value = var.debezium_events_table
+}

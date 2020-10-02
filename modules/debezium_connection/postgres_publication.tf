f05652511d74b86685aa58051e758642ecb4896e
Adam Kerr <ajrkerr@gmail.com>
2020-10-02 16:08:13 -0400

    TASS-388: Rename granter

--- modules/debezium_connection/postgres_publication.tf
+++ modules/debezium_connection/postgres_publication.tf
@@ -1,22 +1,22 @@
 resource "null_resource" "postgres_publication" {
   triggers = {
-    database_address = var.database_address
-    database_name    = var.database_name
-    database_port    = var.database_port
-    events_table     = var.events_table
-    granter_password = var.database_admin_password
-    granter_username = var.database_admin_username
-    postgres_version = var.postgres_version
-    publication_name = var.publication_name
+    database_address        = var.database_address
+    database_name           = var.database_name
+    database_port           = var.database_port
+    events_table            = var.events_table
+    database_admin_password = var.database_admin_password
+    database_admin_username = var.database_admin_username
+    postgres_version        = var.postgres_version
+    publication_name        = var.publication_name
   }
 
   provisioner "local-exec" {
     command = <<EOF
-docker run -e PGPASSWORD="${self.triggers.granter_password}" --rm --entrypoint="" postgres:${self.triggers.postgres_version}-alpine \
+docker run -e PGPASSWORD="${self.triggers.database_admin_password}" --rm --entrypoint="" postgres:${self.triggers.postgres_version}-alpine \
   psql \
   --host ${self.triggers.database_address} \
   --port ${self.triggers.database_port} \
-  --username ${self.triggers.granter_username} \
+  --username ${self.triggers.database_admin_username} \
   --dbname "${self.triggers.database_name}" \
   --command "
 CREATE PUBLICATION debezium_public_events FOR TABLE  WITH (publish = 'insert');

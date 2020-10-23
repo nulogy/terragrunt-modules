7259eab6619f2ee37612e4805a5d97126792ad47
Cameron Ross <cameronr@nulogy.com>
2020-10-23 13:11:50 -0400

    TASS-407: Makes kafka_topics work with TF 0.13

--- modules/kafka_topics/main.tf
+++ modules/kafka_topics/main.tf
@@ -1,6 +1,10 @@
 resource "null_resource" "create_topic" {
   for_each = var.topics
 
+  triggers = {
+    bootstrap_servers = var.bootstrap_servers
+  }
+
   provisioner "local-exec" {
     command = <<EOF
 docker run --rm --entrypoint="" bitnami/kafka:2.5.0 kafka-topics.sh --create \
@@ -16,7 +20,7 @@ EOF
     when    = destroy
     command = <<EOF
 docker run --rm --entrypoint="" bitnami/kafka:2.5.0 kafka-topics.sh --delete \
-  --bootstrap-server ${var.bootstrap_servers} \
+  --bootstrap-server ${self.triggers.bootstrap_servers} \
   --topic ${each.key}
 EOF
   }

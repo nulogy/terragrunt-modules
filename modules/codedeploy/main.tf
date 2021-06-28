cd478eaa6457adda41ff0ff1fb60b73c4675bad7
Daniel Silva <danielsi@nulogy.com>
2021-06-28 14:13:08 -0400

    PM-9255: Change termination wait time to zero because risky deployments will use linear deployments which already gives us some form of wait

--- modules/codedeploy/main.tf
+++ modules/codedeploy/main.tf
@@ -45,7 +45,7 @@ resource "aws_codedeploy_deployment_group" "deployment_group" {
 
     terminate_blue_instances_on_deployment_success {
       action                           = "TERMINATE"
-      termination_wait_time_in_minutes = 5
+      termination_wait_time_in_minutes = 0
     }
   }
 

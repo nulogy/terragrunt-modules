315e5dafd0c4b87664228a402804310f15339464
Jason Schweier <jasons@nulogy.com>
2021-11-02 10:57:48 -0400

    MESSAGEBUS-11: terraform fmt

--- modules/debezium_connection/main.tf
+++ modules/debezium_connection/main.tf
@@ -1,5 +1,5 @@
 locals {
-  replication_slot_name      = (var.replication_slot_name_override == "") ? "${replace(var.connection_name, "-", "_")}_debezium_slot" : var.replication_slot_name_override
+  replication_slot_name = (var.replication_slot_name_override == "") ? "${replace(var.connection_name, "-", "_")}_debezium_slot" : var.replication_slot_name_override
 
   heartbeat_insertion = <<EOF
     INSERT INTO public.${var.events_table}

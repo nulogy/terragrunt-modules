b9026ec504d4268ae191893a136454ef3bac4c0d
Joe Auty <joea@nulogy.com>
2021-11-16 13:51:46 -0800

    support multiple Buildkite queues per account

--- modules/buildkite_elastic_stack/main.tf
+++ modules/buildkite_elastic_stack/main.tf
@@ -26,7 +26,7 @@ resource "aws_security_group" "stack_security_group" {
 }
 
 resource "aws_ssm_parameter" "agent_token" {
-  name        = "/${var.stack_name}/agent-token"
+  name        = "/${var.stack_name}/${var.buildkite_queue}/agent-token"
   description = "Buildkite agent token"
   type        = "SecureString"
   key_id      = var.kms_key

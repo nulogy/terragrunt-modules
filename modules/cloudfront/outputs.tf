eb4b11030d19d8ae3f7e1cc4d3ecd5a58dad4f76
Rafael Moraes <rafops@users.noreply.github.com>
2023-05-26 11:59:06 -0400

    SRE2-1700: Adds arn to output

--- modules/cloudfront/outputs.tf
+++ modules/cloudfront/outputs.tf
@@ -2,6 +2,10 @@ output "id" {
   value = aws_cloudfront_distribution.this.id
 }
 
+output "arn" {
+  value = aws_cloudfront_distribution.this.arn
+}
+
 output "domain_name" {
   value = aws_cloudfront_distribution.this.domain_name
 }
